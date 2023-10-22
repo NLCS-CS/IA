66b8b253149afaf5e5f559093fd84b3cc16da5f0
AUDTJ44 <mptyshot0@gmail.com>
2023-10-22 20:07:09 +0900

    Initial Commit

---
+++ Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by 이명서 on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
